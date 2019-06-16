16ba366e009a393fd353a67f28233464ef7efc3e
Mohit Gupta <mohit.pjr92@gmail.com>
2019-06-16 22:46:48 +0530

    more code

--- Python/Graphs/DFS_traversal.py
+++ Python/Graphs/DFS_traversal.py
@@ -13,41 +13,52 @@ class Graph:
     def printgraph(self):
         print(self.graph)
 
-    def BFS(self, ele):
-        self.visited_list = dict.fromkeys(self.graph.keys(), False)
+    def dfs_traversal(self, start):
+        self.visit_list = dict.fromkeys(self.graph.keys(), False)
 
-        self.stack.append(ele)
-        self.visited_list[ele] = True
-        print('printing :', ele)
-        
+        self.stack.append(start)
+        self.visit_list[start] = True
+        print('Visited ', start)
         while self.stack:
-            self.visit_status = False
-            initial = self.stack[-1]
-            for node in self.graph[initial]:
-                print('Inside stack:', self.stack)
-                if self.visited_list[node] == False:
-                    self.stack.append(node)
-                    self.visited_list[node] = True
-                    print('printing inside:', node)
+            top = self.stack[-1]
+            for ele in self.graph[top]:
+                visit_flag = True
+                if not self.visit_list[ele]:
+                    visit_flag = False
+                    self.stack.append(ele)
+                    print('Visited ', ele)
+                    self.visit_list[ele] = True
                     break
-                else:
-                    self.visit_status = True
-                    self.stack.pop()
+            if visit_flag == True:
+                self.stack.pop()
 
 
 
 g = Graph()
-g.addEdge(0,1)
-g.addEdge(0,2)
+
+g.addEdge(1,2)
 g.addEdge(1,3)
-g.addEdge(1,4)
-g.addEdge(2,4)
 g.addEdge(2,5)
-g.addEdge(3,6)
+g.addEdge(2,4)
+g.addEdge(3,5)
+g.addEdge(4,5)
 g.addEdge(4,6)
-g.addEdge(4,7)
-g.addEdge(5,7)
-g.addEdge(6,8)
-g.addEdge(7,8)
+g.addEdge(5,6)
+
+# g.addEdge(0,1)
+# g.addEdge(0,2)
+# g.addEdge(1,3)
+# g.addEdge(1,4)
+# g.addEdge(2,4)
+# g.addEdge(2,5)
+# g.addEdge(3,6)
+# g.addEdge(4,6)
+# g.addEdge(4,7)
+# g.addEdge(5,7)
+# g.addEdge(6,8)
+# g.addEdge(7,8)
 g.printgraph()
-g.BFS(0)
+g.dfs_traversal(1)
+
+
+
